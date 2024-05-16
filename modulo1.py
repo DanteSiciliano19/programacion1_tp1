12d24890c8df63f60d8621001fb342e35c360f2a
Dante <dantesiciliano10@gmail.com>
2024-05-16 14:17:00 -0300

    programacion1_tp1

---
+++ modulo1.py
@@ -0,0 +1,2 @@
+def agregar_empleado(nombre, apellido, edad, puesto, dni, salario, fecha_ingreso, legajo):
+    return {'Nombre': nombre, 'Apellido': apellido, 'Edad': edad, 'Puesto': puesto, 'Dni': dni, 'Salario': salario, 'Fecha de ingreso': fecha_ingreso, 'Legajo': legajo}
